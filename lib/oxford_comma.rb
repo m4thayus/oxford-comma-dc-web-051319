63aecf5f273d39ca06d1b88b3763ee36bc6597b1
Matt Williams <matt.seongjun@gmail.com>
2019-03-29 17:06:58 -0400

    How does Cambridge feel about this?

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    case array.length
+        when 1
+            array.join
+        when 2
+            array.join(" and ")
+        else
+           last = "and " + array.last 
+           array.pop
+           array << last
+           array.join(", ") 
+        end
 end
